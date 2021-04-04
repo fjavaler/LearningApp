075d81733821d6179ab0c486fe4fb144235bd0af
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-04 14:14:51 -0600

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Fred Javalera on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

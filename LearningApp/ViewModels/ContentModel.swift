0b21ac2574015864e47f8d1bbe4900731917b4ce
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-14 18:51:34 -0600

    - Format code.

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -8,252 +8,252 @@
 import Foundation
 
 class ContentModel: ObservableObject {
+  
+  //MARK: - VARIABLES
+  // List of models
+  @Published var modules = [Module]()
+  
+  // Current module
+  @Published var currentModule: Module?
+  var currentModuleIndex = 0
+  
+  // Current lesson
+  @Published var currentLesson: Lesson?
+  var currentLessonIndex = 0
+  
+  // Current question
+  @Published var currentQuestion: Question?
+  var currentQuestionIndex = 0
+  
+  // Lesson description text
+  @Published var codeText = NSAttributedString()
+  
+  var styleData: Data?
+  
+  // Current selected content and test
+  @Published var currentContentSelected: Int?
+  @Published var currentTestSelected: Int?
+  
+  //MARK: - INITIALIZER
+  init() {
     
-    //MARK: - VARIABLES
-    // List of models
-    @Published var modules = [Module]()
+    // Parse local included JSON data
+    getLocalData()
     
-    // Current module
-    @Published var currentModule: Module?
-    var currentModuleIndex = 0
+    // Download remote JSON file and parse data
+    getRemoteData()
+  }
+  
+  // MARK: - Data Methods
+  
+  /// Parse local included JSON data
+  func getLocalData() {
     
-    // Current lesson
-    @Published var currentLesson: Lesson?
-    var currentLessonIndex = 0
+    // Parse the data.json file
+    // Get a URL to the JSON file
+    let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
     
-    // Current question
-    @Published var currentQuestion: Question?
-    var currentQuestionIndex = 0
-    
-    // Lesson description text
-    @Published var codeText = NSAttributedString()
-    
-    var styleData: Data?
-    
-    // Current selected content and test
-    @Published var currentContentSelected: Int?
-    @Published var currentTestSelected: Int?
-    
-    //MARK: - INITIALIZER
-    init() {
-        
-        // Parse local included JSON data
-        getLocalData()
-        
-        // Download remote JSON file and parse data
-        getRemoteData()
+    do {
+      
+      // Read the file into a data object
+      let jsonData = try Data(contentsOf: jsonUrl!)
+      
+      // Try to decode the JSON into an array of modules
+      let jsonDecoder = JSONDecoder()
+      let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+      
+      // Assign parsed modules to class' modules property
+      self.modules = modules
+    } catch {
+      // Log error
+      fatalError("\(error)")
     }
     
-    // MARK: - Data Methods
+    // Parse the style.html data
+    let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
     
-    /// Parse local included JSON data
-    func getLocalData() {
-        
-        // Parse the data.json file
-        // Get a URL to the JSON file
-        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
-        
-        do {
-            
-            // Read the file into a data object
-            let jsonData = try Data(contentsOf: jsonUrl!)
-            
-            // Try to decode the JSON into an array of modules
-            let jsonDecoder = JSONDecoder()
-            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
-            
-            // Assign parsed modules to class' modules property
-            self.modules = modules
-        } catch {
-            // Log error
-            fatalError("\(error)")
-        }
-        
-        // Parse the style.html data
-        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
-        
-        do {
-            
-            // Read the file into a data object
-            let styleData = try Data(contentsOf: styleUrl!)
-            
-            // Assign parsed data to class' styleData property
-            self.styleData = styleData
-        } catch {
-            fatalError("\(error)")
-        }
+    do {
+      
+      // Read the file into a data object
+      let styleData = try Data(contentsOf: styleUrl!)
+      
+      // Assign parsed data to class' styleData property
+      self.styleData = styleData
+    } catch {
+      fatalError("\(error)")
+    }
+  }
+  
+  /// Download remote JSON file and parse data
+  func getRemoteData() {
+    
+    // String path
+    let urlString = "https://fjavaler.github.io/LearningApp-Data/data2.json"
+    
+    // Create a URL object
+    let url = URL(string: urlString)
+    
+    guard url != nil else {
+      // Couldn't create a URL
+      return
     }
     
-    /// Download remote JSON file and parse data
-    func getRemoteData() {
+    // Create a URLRequest object
+    let request = URLRequest(url: url!)
+    
+    // Get the session and kick off the task
+    let session = URLSession.shared
+    
+    let dataTask = session.dataTask(with: request) { (data, response, error) in
+      
+      // Check if there's an error
+      guard error == nil else {
         
-        // String path
-        let urlString = "https://fjavaler.github.io/LearningApp-Data/data2.json"
+        // There was an error
+        return
+      }
+      
+      // Decode JSON
+      do {
         
-        // Create a URL object
-        let url = URL(string: urlString)
+        // Create JSON decoder
+        let decoder = JSONDecoder()
         
-        guard url != nil else {
-            // Couldn't create a URL
-            return
+        // Decode
+        let modules = try decoder.decode([Module].self, from: data!)
+        
+        // Ensure that main thread is executing appending of parsed modules
+        DispatchQueue.main.async {
+          
+          // Append parsed modules into modules property
+          self.modules += modules
         }
+      } catch {
         
-        // Create a URLRequest object
-        let request = URLRequest(url: url!)
-        
-        // Get the session and kick off the task
-        let session = URLSession.shared
-        
-        let dataTask = session.dataTask(with: request) { (data, response, error) in
-            
-            // Check if there's an error
-            guard error == nil else {
-                
-                // There was an error
-                return
-            }
-            
-            // Decode JSON
-            do {
-                
-                // Create JSON decoder
-                let decoder = JSONDecoder()
-                
-                // Decode
-                let modules = try decoder.decode([Module].self, from: data!)
-                
-              // Ensure that main thread is executing appending of parsed modules
-              DispatchQueue.main.async {
-                
-                // Append parsed modules into modules property
-                self.modules += modules
-              }
-            } catch {
-                
-                // Couldn't parse JSON
-                print(error)
-            }
-            
-            // Handle the response
-            
-        }
-        
-        // Kick off the data task
-        dataTask.resume()
+        // Couldn't parse JSON
+        print(error)
+      }
+      
+      // Handle the response
+      
     }
     
-    // MARK: - Module Navigation Methods
-    func beginModule(_ moduleId: Int) {
+    // Kick off the data task
+    dataTask.resume()
+  }
+  
+  // MARK: - Module Navigation Methods
+  func beginModule(_ moduleId: Int) {
+    
+    // Find the index for this module id
+    for index in 0..<modules.count {
+      
+      if modules[index].id == moduleId {
         
-        // Find the index for this module id
-        for index in 0..<modules.count {
-            
-            if modules[index].id == moduleId {
-                
-                // Found the matching module
-                currentModuleIndex = index
-                break
-            }
-            
-        }
-        
-        // Set the current module
-        currentModule = modules[currentModuleIndex]
+        // Found the matching module
+        currentModuleIndex = index
+        break
+      }
+      
     }
     
-    func beginLesson(_ lessonIndex: Int) {
-        // Check that the lesson index is within range of module lessons
-        if lessonIndex < currentModule!.content.lessons.count {
-            currentLessonIndex = lessonIndex
-        } else {
-            currentLessonIndex = 0
-        }
-        
-        // Set the current lesson and description
-        currentLesson = currentModule!.content.lessons[currentLessonIndex]
-        codeText = addStyling(currentLesson!.explanation)
+    // Set the current module
+    currentModule = modules[currentModuleIndex]
+  }
+  
+  func beginLesson(_ lessonIndex: Int) {
+    // Check that the lesson index is within range of module lessons
+    if lessonIndex < currentModule!.content.lessons.count {
+      currentLessonIndex = lessonIndex
+    } else {
+      currentLessonIndex = 0
     }
     
-    func nextLesson() {
-        // Advance the lesson
-        currentLessonIndex += 1
-        
-        // Check that it is within range
-        if currentLessonIndex < currentModule!.content.lessons.count {
-            
-            // Set the current lesson property and lesson description
-            currentLesson = currentModule!.content.lessons[currentLessonIndex]
-            codeText = addStyling(currentLesson!.explanation)
-        } else {
-            
-            // Reset the lesson state
-            currentLessonIndex = 0
-            currentLesson = nil
-        }
+    // Set the current lesson and description
+    currentLesson = currentModule!.content.lessons[currentLessonIndex]
+    codeText = addStyling(currentLesson!.explanation)
+  }
+  
+  func nextLesson() {
+    // Advance the lesson
+    currentLessonIndex += 1
+    
+    // Check that it is within range
+    if currentLessonIndex < currentModule!.content.lessons.count {
+      
+      // Set the current lesson property and lesson description
+      currentLesson = currentModule!.content.lessons[currentLessonIndex]
+      codeText = addStyling(currentLesson!.explanation)
+    } else {
+      
+      // Reset the lesson state
+      currentLessonIndex = 0
+      currentLesson = nil
+    }
+  }
+  
+  func hasNextLesson() -> Bool {
+    return currentLessonIndex + 1 < currentModule!.content.lessons.count ? true : false
+  }
+  
+  func beginTest(_ moduleId: Int) {
+    // Set the current module
+    beginModule(moduleId)
+    
+    // Set the current question
+    currentQuestionIndex = 0
+    
+    // If the current questions, set the current question to the first one
+    if currentModule?.test.questions.count ?? 0 > 0 {
+      currentQuestion = currentModule!.test.questions[currentQuestionIndex]
+      
+      // Set the question content as well
+      codeText = addStyling(currentQuestion!.content)
+    }
+  }
+  
+  func nextQuestion() {
+    
+    // Advance the question index
+    currentQuestionIndex += 1
+    
+    // Check that it's within the range of questions
+    if currentQuestionIndex < currentModule!.test.questions.count {
+      
+      // Set the current question
+      currentQuestion = currentModule!.test.questions[currentQuestionIndex]
+      codeText = addStyling(currentQuestion!.content)
+    } else {
+      
+      // If not, then reset the properties
+      currentQuestionIndex = 0
+      currentQuestion = nil
+    }
+  }
+  
+  // MARK: - Code Styling
+  private func addStyling(_ htmlString: String) -> NSAttributedString {
+    
+    var resultString = NSAttributedString()
+    var data = Data()
+    
+    // Add the styling data
+    if styleData != nil {
+      data.append(self.styleData!)
     }
     
-    func hasNextLesson() -> Bool {
-        return currentLessonIndex + 1 < currentModule!.content.lessons.count ? true : false
+    // Add the HTML data
+    data.append(Data(htmlString.utf8))
+    
+    // Convert to attributed string
+    do {
+      let attributedString = try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil)
+      
+      resultString = attributedString
+    } catch {
+      print("Couldn't turn HTML into attributed string")
     }
     
-    func beginTest(_ moduleId: Int) {
-        // Set the current module
-        beginModule(moduleId)
-        
-        // Set the current question
-        currentQuestionIndex = 0
-        
-        // If the current questions, set the current question to the first one
-        if currentModule?.test.questions.count ?? 0 > 0 {
-            currentQuestion = currentModule!.test.questions[currentQuestionIndex]
-            
-            // Set the question content as well
-            codeText = addStyling(currentQuestion!.content)
-        }
-    }
-    
-    func nextQuestion() {
-        
-        // Advance the question index
-        currentQuestionIndex += 1
-        
-        // Check that it's within the range of questions
-        if currentQuestionIndex < currentModule!.test.questions.count {
-            
-            // Set the current question
-            currentQuestion = currentModule!.test.questions[currentQuestionIndex]
-            codeText = addStyling(currentQuestion!.content)
-        } else {
-            
-            // If not, then reset the properties
-            currentQuestionIndex = 0
-            currentQuestion = nil
-        }
-    }
-    
-    // MARK: - Code Styling
-    private func addStyling(_ htmlString: String) -> NSAttributedString {
-        
-        var resultString = NSAttributedString()
-        var data = Data()
-        
-        // Add the styling data
-        if styleData != nil {
-            data.append(self.styleData!)
-        }
-        
-        // Add the HTML data
-        data.append(Data(htmlString.utf8))
-        
-        // Convert to attributed string
-        do {
-            let attributedString = try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil)
-            
-            resultString = attributedString
-        } catch {
-            print("Couldn't turn HTML into attributed string")
-        }
-        
-        return resultString
-    }
+    return resultString
+  }
 }
